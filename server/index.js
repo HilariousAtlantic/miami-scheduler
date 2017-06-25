5d770c9ef5d873a937495fb796e5c4c8f72db64d
Luke <lartnak@gmail.com>
2017-06-24 20:43:34 -0700

    setup massive

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const massive = require('massive')
 const bodyParser = require('body-parser')
 
 let app = express()
@@ -6,12 +7,27 @@ let app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-setupRoutes()
+connectDatabase()
+  .then(setupRoutes)
+  .then(startServer)
+  .catch(console.log)
 
-app.listen(3000)
+function connectDatabase () {
+  return massive({
+    host: '127.0.0.1',
+    port: 5432,
+    username: 'miami_scheduler'
+  })
+}
 
-function setupRoutes () {
+function setupRoutes (db) {
   app.get('/', (req, res) => {
     res.send('derp')
+  })
+}
+
+function startServer () {
+  app.listen(3000, () => {
+    console.log('server running at http://localhost:3000')
   })
 }
