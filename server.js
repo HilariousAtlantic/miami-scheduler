0f3875b297fab1952344d5ff9b2f5e03f84ad7aa
Luke Artnak <lartnak@gmail.com>
2018-03-26 19:39:43 -0400

    update shit

--- server.js
+++ server.js
@@ -5,20 +5,22 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare()
-.then(() => {
-  const server = express();
+start();
 
-  server.get('*', (req, res) => {
-    return handle(req, res);
-  })
+async function start() {
+  await app.prepare();
 
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack);
-  process.exit(1);
-});
+  try {
+    const server = express();
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
+  } catch (e) {
+    console.error(e.stack);
+    process.exit(1);
+  }
+}
