4c590a8d1302459994a598973ed6dd1a015e75e5
Luke Artnak <lartnak@gmail.com>
2018-03-26 21:10:28 -0400

    filter course search by term

--- server/api.js
+++ server/api.js
@@ -4,15 +4,12 @@ module.exports = function(db) {
 
   router.get('/search', async (req, res) => {
     const { term, q } = req.query;
-    const courses = await db.courses.search(
-      {
+    const courses = await db.courses
+      .search({
         term: q,
         fields: ['subject', 'number', 'title', 'description']
-      },
-      {
-        term
-      }
-    );
+      })
+      .then(courses => courses.filter(course => course.term === term));
 
     res.json({ courses });
   });
