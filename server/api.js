f3cfb5fd49ba70045ad00b0f4ef15deaeeb94117
Luke Artnak <lartnak@gmail.com>
2018-03-26 20:50:41 -0400

    add search

--- server/api.js
+++ server/api.js
@@ -2,8 +2,19 @@ exports.default = function(db) {
   var express = require('express');
   var router = express.Router();
 
-  router.get('/search', (req, res) => {
-    const { q } = req.query;
+  router.get('/search', async (req, res) => {
+    const { term, q } = req.query;
+    const courses = db.courses.search(
+      {
+        term: q,
+        fields: ['subject', 'number', 'title', 'description']
+      },
+      {
+        term
+      }
+    );
+
+    res.json({ courses });
   });
 
   return router;
--- server/database.js
+++ server/database.js
@@ -3,14 +3,13 @@ const massive = require('massive');
 require('dotenv').config();
 
 exports.default = {
-  connectDatabase: async function() {
-    const db = await massive({
+  async connectDatabase() {
+    return await massive({
       host: process.env.DATABASE_HOST,
       port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD
     });
-    return db.reload();
   }
 };
