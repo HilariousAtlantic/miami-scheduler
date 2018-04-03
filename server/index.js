c7cb0fe12fa1e5f5a94547e4fa34d8e8f2c6319e
Luke Artnak <lartnak@gmail.com>
2018-04-03 19:28:12 -0400

    make project deployable

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ async function start() {
 
     server.get('*', (req, res) => handle(req, res));
 
-    server.listen(3000, err => {
+    server.listen(process.env.PORT || 3000, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
